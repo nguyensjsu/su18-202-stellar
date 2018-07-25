e8bd5cace099d6d4a25bb0179262974bca380ef4
MudritaC <mudrita.chaturvedi@sjsu.edu>
2018-07-25 00:53:24 -0700

    Implemented service for makePayment

--- starbucksApp/src/main/java/org/CMPE202/starbucks/service/impl/PaymentServiceImpl.java
+++ starbucksApp/src/main/java/org/CMPE202/starbucks/service/impl/PaymentServiceImpl.java
@@ -1,11 +1,32 @@
 package org.CMPE202.starbucks.service.impl;
 
 import org.CMPE202.starbucks.dao.impl.PaymentDaoImpl;
+import org.CMPE202.starbucks.model.Payment;
+import org.CMPE202.starbucks.model.Store;
+import org.CMPE202.starbucks.responseVo.StoreReponseVo;
 import org.CMPE202.starbucks.service.IPaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements IPaymentService {
 
+    @Autowired
     private PaymentDaoImpl paymentDao;
+
+    public String makePayment(Payment payment) {
+
+        String uniqueID = UUID.randomUUID().toString();
+        payment.setPaymentId(uniqueID);
+        payment.setPaymentSuccessful(true);
+
+        return paymentDao.makePayment(payment);
+
+    }
+
+
+
 }
