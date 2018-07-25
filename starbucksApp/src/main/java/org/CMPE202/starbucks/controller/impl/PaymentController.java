be665d44e090a2b07882056489271f183cf01f74
MudritaC <mudrita.chaturvedi@sjsu.edu>
2018-07-25 00:54:57 -0700

    implemented rest api for make payment

--- starbucksApp/src/main/java/org/CMPE202/starbucks/controller/impl/PaymentController.java
+++ starbucksApp/src/main/java/org/CMPE202/starbucks/controller/impl/PaymentController.java
@@ -3,14 +3,28 @@ package org.CMPE202.starbucks.controller.impl;
 
 import org.CMPE202.starbucks.constant.StarbucksConstants;
 import org.CMPE202.starbucks.controller.IPaymentController;
+import org.CMPE202.starbucks.model.Payment;
+import org.CMPE202.starbucks.responseVo.StoreReponseVo;
 import org.CMPE202.starbucks.service.impl.PaymentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value= StarbucksConstants.PAYMENT)
 public class PaymentController implements IPaymentController{
 
+    @Autowired
     private PaymentServiceImpl paymentService;
+
+
+    @RequestMapping(value= StarbucksConstants.MAKE_PAYMENT, method= RequestMethod.POST)
+    public @ResponseBody
+    String makePayment (@RequestBody Payment payment) {
+
+        return paymentService.makePayment(payment);
+
+    }
+    
+
 }
