e3b2bf4a7713fbe0a3fca2f52d7ef333060505e1
MudritaC <mudrita.chaturvedi@sjsu.edu>
2018-07-25 00:47:11 -0700

    Payment model update

--- starbucksApp/src/main/java/org/CMPE202/starbucks/model/Payment.java
+++ starbucksApp/src/main/java/org/CMPE202/starbucks/model/Payment.java
@@ -1,4 +1,54 @@
 package org.CMPE202.starbucks.model;
 
+import java.util.Date;
+
 public class Payment {
+
+    private String paymentId;
+    private String orderId;
+    private String cardId;
+    private boolean isPaymentSuccessful;
+    private String paymentDate;
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+    public boolean isPaymentSuccessful() {
+        return isPaymentSuccessful;
+    }
+
+    public void setPaymentSuccessful(boolean paymentSuccessful) {
+        isPaymentSuccessful = paymentSuccessful;
+    }
 }
