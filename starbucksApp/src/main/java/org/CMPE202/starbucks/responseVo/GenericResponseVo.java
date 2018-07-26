0c78d64455d7ad3b4490c38f96268e4dee341e30
MudritaC <mudrita.chaturvedi@sjsu.edu>
2018-07-26 13:29:27 -0700

    added genericVo and userSessionVo

---
+++ starbucksApp/src/main/java/org/CMPE202/starbucks/responseVo/GenericResponseVo.java
@@ -0,0 +1,15 @@
+package org.CMPE202.starbucks.responseVo;
+
+public class GenericResponseVo {
+
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
