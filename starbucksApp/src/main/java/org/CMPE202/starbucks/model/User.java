a66ea8bc373197b1e7c35e5f3ae40a2a02c4026e
cmpe202-anjanakamath92 <anjana.kamathmiyar@sjsu.edu>
2018-07-27 00:50:28 -0700

    user model

--- starbucksApp/src/main/java/org/CMPE202/starbucks/model/User.java
+++ starbucksApp/src/main/java/org/CMPE202/starbucks/model/User.java
@@ -5,59 +5,55 @@ import java.io.Serializable;
 public class User implements Serializable{
 	private static final long serialVersionUID = -7788619177798333712L;
 	private String userID;
-	private String userName;
-	//private String lastname;
-	private String createdDate;
-	//private String modifiedDate;
 	private String userPin;
-	//private String confirmPassword;
+	private String userName;
+	private String createdDate;
 
-	/*public String getConfirmPassword() {
-		return confirmPassword;
+	public static long getSerialVersionUID() {
+		return serialVersionUID;
 	}
 
-	public void setConfirmPassword(String confirmPassword) {
-		this.confirmPassword = confirmPassword;
-	}*/
-
-
-	public String getPin() {
-		return userPin;
-	}
-
-	public void setPin(String userPin) {
-		this.userPin = userPin;
-	}
 	public String getUserID() {
 		return userID;
 	}
+
 	public void setUserID(String userID) {
 		this.userID = userID;
 	}
-	public String getUsername() {
+
+	public String getUserPin() {
+		return userPin;
+	}
+
+	public void setUserPin(String userPin) {
+		this.userPin = userPin;
+	}
+
+	public String getUserName() {
 		return userName;
 	}
-	public void setUsername(String firstname) {
+
+	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	/*public String getLastname() {
-		return lastname;
-	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}*/
+
 	public String getCreatedDate() {
 		return createdDate;
 	}
+
 	public void setCreatedDate(String createdDate) {
 		this.createdDate = createdDate;
 	}
-	/*public String getModifiedDate() {
-		return modifiedDate;
-	}
-	public void setModifiedDate(String modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}*/
+
+
+//	private String firstname;
+//	private String lastname;
+//	private String createdDate;
+//	private String modifiedDate;
+//	private String password;
+//	private String confirmPassword;
+
+
 	
 
 }
