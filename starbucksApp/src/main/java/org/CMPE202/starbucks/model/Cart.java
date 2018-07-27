13317583e1347f19f10d2e9afcaf4457eefa18fa
cmpe202-cnvdya <vidyacoimbatorenandkumar@sjsu.edu>
2018-07-27 03:05:07 -0700

    adding model for cart

--- starbucksApp/src/main/java/org/CMPE202/starbucks/model/Cart.java
+++ starbucksApp/src/main/java/org/CMPE202/starbucks/model/Cart.java
@@ -1,4 +1,54 @@
 package org.CMPE202.starbucks.model;
 
-public class Cart {
+public class Cart  {
+
+    private String cartId;
+    private String userId;
+    private String itemId;
+    private String quantity;
+
+    public String getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(String cartId) {
+        this.cartId = cartId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    private double total;
+
+
+
 }
