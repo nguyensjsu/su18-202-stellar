773a3e101d41fb434fa77540f1a9e92f9fa00613
cnvdya <cnvdya@gmail.com>
2018-07-27 19:50:04 -0700

    adding changes for view order

--- starbucksApp/src/main/java/org/CMPE202/starbucks/dao/IOrderDao.java
+++ starbucksApp/src/main/java/org/CMPE202/starbucks/dao/IOrderDao.java
@@ -2,7 +2,11 @@ package org.CMPE202.starbucks.dao;
 
 import org.CMPE202.starbucks.model.Order;
 
+import java.util.List;
+
 public interface IOrderDao {
 
     public String placeOrder(Order order);
+
+    public List<Order> viewOrders(Order order);
 }
