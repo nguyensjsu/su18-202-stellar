7b4f6ad25f2d8bf19279e5baccf41018e1a9f5a4
cnvdya <cnvdya@gmail.com>
2018-07-27 19:52:16 -0700

    adding changes for view order

--- starbucksApp/src/main/java/org/CMPE202/starbucks/service/impl/OrderServiceImpl.java
+++ starbucksApp/src/main/java/org/CMPE202/starbucks/service/impl/OrderServiceImpl.java
@@ -1,12 +1,25 @@
 package org.CMPE202.starbucks.service.impl;
 
 
+import org.CMPE202.starbucks.dao.impl.CartDaoImpl;
 import org.CMPE202.starbucks.dao.impl.OrderDaoImpl;
+import org.CMPE202.starbucks.model.Card;
+import org.CMPE202.starbucks.model.Order;
 import org.CMPE202.starbucks.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements IOrderService {
 
+    @Autowired
     private OrderDaoImpl orderDao;
+
+    public List<Order> viewAllOrders(Order order) {
+
+        List<Order> orderList = orderDao.viewOrders(order);
+        return orderList;
+    }
 }
