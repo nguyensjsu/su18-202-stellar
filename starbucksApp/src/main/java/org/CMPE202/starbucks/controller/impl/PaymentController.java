cd8ad905bbd0e882d50ecb41cd65937cd11d5177
MudritaC <mudrita.chaturvedi@sjsu.edu>
2018-07-27 19:36:00 -0700

    updated payment controller

--- starbucksApp/src/main/java/org/CMPE202/starbucks/controller/impl/PaymentController.java
+++ starbucksApp/src/main/java/org/CMPE202/starbucks/controller/impl/PaymentController.java
@@ -4,6 +4,7 @@ package org.CMPE202.starbucks.controller.impl;
 import org.CMPE202.starbucks.constant.StarbucksConstants;
 import org.CMPE202.starbucks.controller.IPaymentController;
 import org.CMPE202.starbucks.model.Payment;
+import org.CMPE202.starbucks.responseVo.GenericResponseVo;
 import org.CMPE202.starbucks.responseVo.StoreReponseVo;
 import org.CMPE202.starbucks.service.impl.PaymentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,7 @@ public class PaymentController implements IPaymentController{
 
     @RequestMapping(value= StarbucksConstants.MAKE_PAYMENT, method= RequestMethod.POST)
     public @ResponseBody
-    String makePayment (@RequestBody Payment payment) {
+    GenericResponseVo makePayment (@RequestBody Payment payment) {
 
         return paymentService.makePayment(payment);
 
