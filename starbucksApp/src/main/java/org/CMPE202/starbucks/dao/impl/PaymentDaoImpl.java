722e9c06ff55aab9fe183b34b3289ec710790a8b
MudritaC <mudrita.chaturvedi@sjsu.edu>
2018-07-27 19:34:14 -0700

    updated payment logic

--- starbucksApp/src/main/java/org/CMPE202/starbucks/dao/impl/PaymentDaoImpl.java
+++ starbucksApp/src/main/java/org/CMPE202/starbucks/dao/impl/PaymentDaoImpl.java
@@ -1,6 +1,8 @@
 package org.CMPE202.starbucks.dao.impl;
 
 import org.CMPE202.starbucks.dao.IPaymentDao;
+import org.CMPE202.starbucks.model.Card;
+import org.CMPE202.starbucks.model.Order;
 import org.CMPE202.starbucks.model.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,12 +11,19 @@ import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class PaymentDaoImpl implements IPaymentDao {
 
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private CardDaoImpl cardDao;
+
+    @Autowired
+    private OrderDaoImpl orderDao;
 
     public DataSource getDataSource() {
         return dataSource;
@@ -37,6 +46,17 @@ public class PaymentDaoImpl implements IPaymentDao {
     @Override
     public String makePayment(Payment payment) {
 
+        Order order =new Order();
+        String orderId = UUID.randomUUID().toString();
+        order.setOrderId(orderId);
+        order.setUserId(payment.getUserId());
+        order.setAmount(payment.getTotal());
+        orderDao.placeOrder(order);
+
+
+        Card card = cardDao.viewCardDetails(payment.getCardNumber());
+
+
         String sql ="INSERT INTO PAYMENT("+
                 "paymentId,"+
                 "orderId,"+
@@ -45,11 +65,11 @@ public class PaymentDaoImpl implements IPaymentDao {
                 "paymentDate)"+
                 "VALUES (?,?,?,?,?)";
 
-        Object[] params = new Object[] {payment.getPaymentId(),payment.getOrderId(),payment.getCardId(),payment.isPaymentSuccessful(), new Date()};
+        Object[] params = new Object[] {payment.getPaymentId(),orderId,card.getCardId(),payment.isPaymentSuccessful(), new Date()};
         try {
             jdbcTemplate.update(sql, params);
             try{
-                String str =deductAmountFromCard(payment);
+                String str =deductAmountFromCard(payment,orderId);
                 if(str.equals("Balance updated Successfully")){
                     return("Payment done Successfully");
                 }
@@ -69,13 +89,13 @@ public class PaymentDaoImpl implements IPaymentDao {
 
     }
 
-    private String deductAmountFromCard(Payment payment){
+    private String deductAmountFromCard(Payment payment, String orderId){
 
         //get current balance
-        double balance =getCurrentBalance(payment.getCardId());
+        double balance =getCurrentBalance(payment.getCardNumber());
 
         //get amount to be paid
-        double amount =getAmountToPay(payment.getOrderId());
+        double amount =getAmountToPay(orderId);
 
         double newBalance = balance-amount;
 
@@ -83,8 +103,8 @@ public class PaymentDaoImpl implements IPaymentDao {
         if(newBalance >=0){
             String sql = "UPDATE CARD SET "+
                     "balance = ? "+
-                    "WHERE cardId = ?";
-            Object[] params = new Object[] {newBalance,payment.getCardId()};
+                    "WHERE cardNumber = ?";
+            Object[] params = new Object[] {newBalance,payment.getCardNumber()};
 
             try {
                 jdbcTemplate.update(sql, params);
@@ -102,10 +122,10 @@ public class PaymentDaoImpl implements IPaymentDao {
         return("Update Failed");
     }
 
-    private double getCurrentBalance(String cardId){
+    private double getCurrentBalance(String cardNumber){
 
-        String sql = "SELECT balance FROM CARD WHERE cardId = ? ";
-        Object[] params = new Object[] {cardId};
+        String sql = "SELECT balance FROM CARD WHERE cardNumber = ? ";
+        Object[] params = new Object[] {cardNumber};
 
         double balance=(double)jdbcTemplate.queryForObject(sql,params,Double.class);
             return balance;
